4ef60b207f11d91d1e1581854c87d287bca5620c
Богдан Савченко <91850335+SSovyshka@users.noreply.github.com>
2025-05-25 18:21:16 +0300

    Update main.cpp

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -4,12 +4,17 @@
 #include <ctime>
 #include <iostream>
 #include <string>
+#include <cmath>
 
 const int CELL_SIZE = 50;
 const int GRID_SIZE = 10;
 
 int grid[GRID_SIZE][GRID_SIZE];
 std::map<int, int> frequencies;
+POINT lastMousePos = { 0, 0 };
+
+std::vector<wchar_t> typedChars;
+int charX = 100, charY = 100;
 
 using namespace std;
 
@@ -19,7 +24,7 @@ void GenerateGrid() {
         for (int j = 0; j < GRID_SIZE; j++) {
             int value = rand() % 10;
             grid[i][j] = value;
-            frequencies[value]++; 
+            frequencies[value]++;
         }
     }
 }
@@ -36,12 +41,11 @@ void DrawGrid(HDC hdc) {
             FillRect(hdc, &rect, hBrush);
             DeleteObject(hBrush);
 
-            
             wstring text = to_wstring(value);
             SetTextColor(hdc, RGB(255, 255, 255));
             SetBkMode(hdc, TRANSPARENT);
 
-            RECT textRect = { x * CELL_SIZE, y * CELL_SIZE, (x + 1) * CELL_SIZE, (y + 1) * CELL_SIZE };
+            RECT textRect = rect;
             DrawText(hdc, text.c_str(), -1, &textRect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
         }
     }
@@ -53,7 +57,7 @@ void DrawCharts(HDC hdc) {
     int centerX = GRID_SIZE * CELL_SIZE + 150;
     int centerY = 200;
     int radius = 100;
-    int innerRadius = 70; 
+    int innerRadius = 70;
 
     for (auto& pair : frequencies) {
         int percent = (pair.second * 360) / total;
@@ -70,7 +74,6 @@ void DrawCharts(HDC hdc) {
     }
 
     int ringChartCenterY = centerY + 250;
-
     startAngle = 0;
     for (auto& pair : frequencies) {
         int percent = (pair.second * 360) / total;
@@ -78,7 +81,6 @@ void DrawCharts(HDC hdc) {
 
         HBRUSH hBrush = CreateSolidBrush(RGB(rand() % 256, rand() % 256, rand() % 256));
         SelectObject(hdc, hBrush);
-
         Pie(hdc, centerX - radius, ringChartCenterY - radius, centerX + radius, ringChartCenterY + radius,
             centerX + radius * cos(startAngle * 3.14 / 180),
             ringChartCenterY - radius * sin(startAngle * 3.14 / 180),
@@ -95,14 +97,74 @@ void DrawCharts(HDC hdc) {
     DeleteObject(whiteBrush);
 }
 
+void ShowDeviceInfo(HDC hdc) {
+    int widthMM = GetDeviceCaps(hdc, HORZSIZE);
+    int heightMM = GetDeviceCaps(hdc, VERTSIZE);
+
+    int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+    int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+    TEXTMETRIC tm;
+    GetTextMetrics(hdc, &tm);
+
+    bool hasMouse = GetSystemMetrics(SM_MOUSEPRESENT);
+    int buttons = GetSystemMetrics(SM_CMOUSEBUTTONS);
+
+    RECT infoRect = { 550, 10, 780, 300 };
+    SetTextColor(hdc, RGB(0, 0, 0));
+    SetBkMode(hdc, TRANSPARENT);
+
+    wchar_t buffer[512];
+    swprintf(buffer, 512,
+        L"Монітор: %d x %d мм\nРоздільна здатність: %d x %d пікселів\nШрифт: висота %d, міжстрок %d\nМиша: є=%d, кнопок=%d",
+        widthMM, heightMM, screenWidth, screenHeight, tm.tmHeight, tm.tmExternalLeading, hasMouse, buttons);
+    DrawText(hdc, buffer, -1, &infoRect, DT_LEFT);
+}
+
+void DrawTypedChars(HDC hdc) {
+    for (size_t i = 0; i < typedChars.size(); ++i) {
+        wchar_t ch[2] = { typedChars[i], 0 };
+        TextOut(hdc, charX + (i * 20), charY, ch, 1);
+    }
+}
+
+void RandomCharAtMouse(HWND hwnd) {
+    wchar_t ch = L'!' + rand() % 94;
+    HDC hdc = GetDC(hwnd);
+    POINT pt;
+    GetCursorPos(&pt);
+    ScreenToClient(hwnd, &pt);
+    wchar_t s[2] = { ch, 0 };
+    TextOut(hdc, pt.x, pt.y, s, 1);
+    ReleaseDC(hwnd, hdc);
+}
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     switch (msg) {
+    case WM_CHAR:
+        typedChars.push_back((wchar_t)wParam);
+        charX = rand() % 500;
+        charY = rand() % 500;
+        InvalidateRect(hwnd, NULL, TRUE);
+        break;
+
+    case WM_MOUSEMOVE: {
+        POINT currentPos = { LOWORD(lParam), HIWORD(lParam) };
+        int dx = currentPos.x - lastMousePos.x;
+        int dy = currentPos.y - lastMousePos.y;
+        if (sqrt(dx * dx + dy * dy) > 100) {
+            RandomCharAtMouse(hwnd);
+            lastMousePos = currentPos;
+        }
+    } break;
+
     case WM_PAINT: {
         PAINTSTRUCT ps;
         HDC hdc = BeginPaint(hwnd, &ps);
         DrawGrid(hdc);
         DrawCharts(hdc);
+        ShowDeviceInfo(hdc);
+        DrawTypedChars(hdc);
         EndPaint(hwnd, &ps);
     } break;
 
@@ -113,26 +175,22 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     default:
         return DefWindowProc(hwnd, msg, wParam, lParam);
     }
+    return 0;
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     GenerateGrid();
 
-    for (int i = 0; i < GRID_SIZE; i++) {
-        for (int j = 0; j < GRID_SIZE; j++) {
-            cout << grid[i][j] << " ";
-        }
-        cout << "\n";
-    }
-
     WNDCLASS wc = {};
     wc.lpfnWndProc = WndProc;
     wc.hInstance = hInstance;
-    wc.lpszClassName = L"Lab2";
+    wc.lpszClassName = L"Lab2Full";
     RegisterClass(&wc);
-    HWND hwnd = CreateWindowEx(0, L"Lab2", L"Lab2", WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, 800, 600, NULL, NULL, hInstance, NULL);
+
+    HWND hwnd = CreateWindowEx(0, L"Lab2Full", L"Lab2 - Повна версiя", WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 1000, 700, NULL, NULL, hInstance, NULL);
     ShowWindow(hwnd, nCmdShow);
+
     MSG msg = {};
     while (GetMessage(&msg, NULL, 0, 0)) {
         TranslateMessage(&msg);
